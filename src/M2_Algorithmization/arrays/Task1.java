23e59b4060c3e77633f5b26cd6379a528f249571
SergeyTsinin <sergey.tsynin@gmail.com>
2020-06-10 19:46:45 +0300

    Solution for arrays task1

--- src/M2_Algorithmization/arrays/Task1.java
+++ src/M2_Algorithmization/arrays/Task1.java
@@ -1,4 +1,15 @@
-package M2_Algorithmization;
+package M2_Algorithmization.arrays;
 
 public class Task1 {
+
+    /*
+     * В массив A[N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному К.
+     */
+    private static int sumOfElements(int[] sequence, int k) {
+        int sum = 0;
+        for (int value : sequence) {
+            if (value % k == 0) sum += value;
+        }
+        return sum;
+    }
 }
