0b307daa9955af604496164cced90cc7e38d5d9f
SergeyTsinin <sergey.tsynin@gmail.com>
2020-06-11 14:11:15 +0300

    Solution for arrays task2, remove trash

--- src/M2_Algorithmization/arrays/Task2.java
+++ src/M2_Algorithmization/arrays/Task2.java
@@ -1,14 +1,6 @@
 package M2_Algorithmization.arrays;
 
-import java.util.Arrays;
-
 public class Task2 {
-    public static void main(String[] args) {
-        int[] seq = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-        System.out.println(Arrays.toString(seq));
-        System.out.println(limitArray(seq, 6));
-        System.out.println(Arrays.toString(seq));
-    }
 
     /*
      * Дана последовательность действительных чисел а1 ,а2 ,..., ап.
